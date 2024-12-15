abca71d7a3ec4a0e251f2fec0a1c534134d88a7c
33QWERTY33 <curlejo.919@gmail.com>
2024-12-15 01:18:43 -0500

    decoupled python execution accomplished

--- GUIPyDebugger/editor/views.py
+++ GUIPyDebugger/editor/views.py
@@ -1,7 +1,39 @@
+import io
+import json
+import sys
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 # Display the text box for editing
+@csrf_exempt
 def editor(request):
+    if request.method == "POST":
+
+        output = ""
+
+        data = json.loads(request.body)
+
+        code = data.get('code')
+
+        output_buffer = io.StringIO()
+
+        sys.stdout = output_buffer
+
+        try:
+            exec(code)
+
+            sys.stdout.flush()
+
+            output = output_buffer.getvalue()
+            
+        except Exception as e:
+            output = str(e)
+
+        sys.stdout = sys.__stdout__
+        
+        return JsonResponse({"output": output})
+
     return render(request, 'editor/editor.html')
--- GUIPyDebugger/manage.py
+++ GUIPyDebugger/manage.py
@@ -2,7 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import zmq
+import threading
 
+new_code = threading.Event()
 
 def main():
     """Run administrative tasks."""
@@ -15,8 +18,74 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    
+    # INSERTING ZMQ SOCKET THREADS HERE
+    rep_thread = threading.Thread(target=create_rep_socket, name="REPSocketThread")
+    req_thread = threading.Thread(target=create_req_socket, name="REQSocketThread")
+
+    if not rep_thread.is_alive():
+        rep_thread.start()
+        req_thread.start()
+
     execute_from_command_line(sys.argv)
 
+def create_req_socket():
+    # create a ZMQ context
+    context = zmq.Context()
+
+    # Create a REQ (request) socket
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5555") # connect to 5554
+    socket.setsockopt(zmq.LINGER, 0)
+
+    while True:
+        try:
+            code = ""
+            # wait for user input to be sent
+            new_code.wait()
+
+            # copy code in file
+            with open(r"./static/code_request.py", "r") as code:
+                for line in code.readlines():
+                    code += line
+
+            # Temporary teardown logic
+            if code.lower() == 'exit':
+                context.destroy()
+                break
+
+            # Send the code to the server
+            socket.send_string(code)
+
+            # Handle response
+            response = socket.recv_string()
+            print(f"server response: {response}")
+
+            new_code.clear()
+
+        except KeyboardInterrupt:
+            break
+
+def create_rep_socket():
+    # create a ZMQ context
+    context = zmq.Context()
+
+    # create a REP (reply) socket
+    socket = context.socket(zmq.REP)
+    socket.bind('tcp://*:5555') #bind to port 5555
+    socket.setsockopt(zmq.LINGER, 0)
+
+    while True:
+        # wait for a message
+        message = socket.recv_string()
+
+        try:
+            # Execute the received message
+            result = eval(message) # use exec(message) for multi-line code
+            socket.send_string(f"Result: {result}")
+        except Exception as e:
+            # send back the error message if execution fails
+            socket.send_string(f"Error: {str(e)}")
 
 if __name__ == '__main__':
     main()
